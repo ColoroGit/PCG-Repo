0bdcc671fe8e087205a6550ccf6ac97b6d903d19
Valentine Sierra <valesierracuevas@gmail.com>
2025-10-18 13:00:15 -0300

    Eliminación librerías sin usar
    Daban error de build, pero no de compilación

--- Assets/Scripts/GenAlg.cs
+++ Assets/Scripts/GenAlg.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static Unity.VisualScripting.LudiqRootObjectEditor;
+// using static Unity.VisualScripting.LudiqRootObjectEditor;
 
 public class GenAlg : MonoBehaviour
 {
